aee3ef805487c6ea7981b0b8d31321a9a33e8837
Paulino <paulinomendes03@gmail.com>
2025-05-21 20:20:49 -0300

    inicio do projeto - sem alteracoes

---
+++ src/main/java/com/consultas/api_consultas/ApiDeConsultasMedicasApplication.java
@@ -0,0 +1,13 @@
+package com.consultas.api_consultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeConsultasMedicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeConsultasMedicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/consultas/api_consultas/ApiDeConsultasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consultas.api_consultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeConsultasMedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
