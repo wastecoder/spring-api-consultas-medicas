5e3d79f4888b8575d9471b91ad8428aad05e7dd7
Paulino <paulinomendes03@gmail.com>
2025-05-23 19:42:26 -0300

    feat(exceptions): adiciona exception personalizada e faz tratamento de exceptions de globalmente

---
+++ src/main/java/com/consultas/api_consultas/exceptions/BusinessRuleException.java
@@ -0,0 +1,7 @@
+package com.consultas.api_consultas.exceptions;
+
+public class BusinessRuleException extends RuntimeException {
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+}
