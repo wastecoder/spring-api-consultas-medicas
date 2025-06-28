61e8bdddc3ccd8f3d0f45ca28d10168f4174fe40
Pedro <pedromendes7182@gmail.com>
2025-06-28 15:45:22 -0300

    refactor(relatorio): remove RelatorioController geral e organiza por grupo

--- src/main/java/com/consultas/api_consultas/services/RelatorioService.java
+++
@@ -1,9 +0,0 @@
-package com.consultas.api_consultas.services;
-
-import com.consultas.api_consultas.dtos.respostas.ConsultasPorStatusDto;
-
-public interface RelatorioService {
-
-    ConsultasPorStatusDto gerarRelatorioConsultasPorStatus();
-
-}
