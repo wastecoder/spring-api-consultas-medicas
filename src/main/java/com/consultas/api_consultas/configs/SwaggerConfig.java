864cd9e233d1b6ea9318889ee788e5a28bb3dd76
Pedro <pedromendes7182@gmail.com>
2025-07-07 04:09:32 -0300

    chore(docs): configura basic auth no Swagger

--- src/main/java/com/consultas/api_consultas/configs/SwaggerConfig.java
+++ src/main/java/com/consultas/api_consultas/configs/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package com.consultas.api_consultas.configs;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +15,8 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final String schemeName = "basicAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("API de Agendamentos Médicos")
@@ -24,6 +29,16 @@ public class SwaggerConfig {
                         .license(new License()
                                 .name("Apache 2.0")
                                 .url("https://www.apache.org/licenses/LICENSE-2.0.html"))
+                )
+                .addSecurityItem(new SecurityRequirement().addList(schemeName))
+                .components(new Components()
+                        .addSecuritySchemes(
+                                schemeName,
+                                new SecurityScheme()
+                                        .name(schemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("basic")
+                        )
                 );
     }
 
