80af1fd11dda14062f7ef8b41a1a71e786125f91
Pedro <pedromendes7182@gmail.com>
2025-09-03 18:43:35 -0300

    feat(SecurityConfig): ativa CORS no Filter Chain

--- src/main/java/com/consultas/api_consultas/configs/SecurityConfig.java
+++ src/main/java/com/consultas/api_consultas/configs/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -44,6 +45,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, CustomAccessDeniedHandler accessDeniedHandler) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable) // Usado em cookies
                 .authorizeHttpRequests(auth -> auth
                         // TODOS: Swagger
