76bf60dbd726ce21839066a07d373437639686d2
Pedro <pedromendes7182@gmail.com>
2026-05-11 21:01:39 -0300

    test: aplica perfil test em ApiDeConsultasMedicasApplicationTests

--- src/test/java/com/consultas/api_consultas/ApiDeConsultasMedicasApplicationTests.java
+++ src/test/java/com/consultas/api_consultas/ApiDeConsultasMedicasApplicationTests.java
@@ -2,8 +2,10 @@ package com.consultas.api_consultas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApiDeConsultasMedicasApplicationTests {
 
 	@Test
